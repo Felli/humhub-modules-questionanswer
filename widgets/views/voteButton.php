e6891a099f2d80eb15fd7ba5a70348aa820e0e00
QUT <benmag@users.noreply.github.com>
2016-01-20 16:11:24 +1000

    fix: vote buttons

--- widgets/views/voteButton.php
+++ widgets/views/voteButton.php
@@ -4,12 +4,14 @@
 ]); ?>
 
     <?php if(!isset($btnClass)) $btnClass = "btn btn-default btn-xs"; ?>
+    <div style="display:none;">
     <?php echo $form->field($model, 'should_open_question')->hiddenInput(['value' => $should_open_question])->label(false); ?>
     <?php echo $form->field($model, 'post_id')->hiddenInput(['value' => $post_id])->label(false); ?>
     <?php echo $form->field($model, 'vote_on')->hiddenInput(['value' => $vote_on])->label(false); ?>
     <?php echo $form->field($model, 'vote_type')->hiddenInput(['value' => $vote_type])->label(false); ?>
+    </div>
     <?php echo \yii\helpers\Html::tag('button', '<i class="fa fa-angle-'.$vote_type.'"></i>', array(
-        'options' => array('class'=> $btnClass . " " . $class, 'type'=>'submit', 'style' => 'margin-top:5px;')
+        'class'=> $btnClass . " " . $class, 'type'=>'submit', 'style' => 'margin-top:5px;'
     )); ?>
 
 <?php \yii\widgets\ActiveForm::end(); ?>
