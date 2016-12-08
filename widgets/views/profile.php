0dcc6543d7d48bc68ee9149b8b58f6c6493150f0
VolkovDenis <volkovdenya@gmail.com>
2016-12-08 19:14:06 +0200

    fixed qa

--- widgets/views/profile.php
+++ widgets/views/profile.php
@@ -17,7 +17,7 @@ use humhub\modules\karma\models\KarmaUser;
 
     <div class="row">
         <div class="col-xs-12 qanda-profile-timestamp">
-            <small>posted <?php \yii\helpers\Html::encode(1); ?></small>
+            <small>posted <?php echo \humhub\widgets\TimeAgo::widget(['timestamp' => $timestamp]); ?></small>
         </div>
     </div>
     <div class="row">
