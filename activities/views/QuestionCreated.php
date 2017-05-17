038ffb7ad12ed00afcca85718aebbb6a1ca5d337
Katarzyna Fedevych <katarzynan@gmail.com>
2017-05-17 10:07:42 +0200

    TC-205 - minor HTML adjustments

--- activities/views/QuestionCreated.php
+++ activities/views/QuestionCreated.php
@@ -1,12 +1,9 @@
 <?php
 
 use yii\helpers\Html;
+
+echo Yii::t('app', '<i class="fa fa-stack-exchange" style="margin-right: 5px;color: #ffdf2c;vertical-align: middle"></i> {userName} asked {answer}.', array(
+    '{userName}' => '<strong>' . Html::encode($originator->displayName) . '</strong>',
+    '{answer}' => Html::encode($source->getContentDescription())
+));
 ?>
-<div class="question-activity">
-    <?php
-        echo Yii::t('app', '<i class="fa fa-stack-exchange" style="margin-right: 5px;color: #ffdf2c;vertical-align: middle"></i> {userName} asked {answer}.', array(
-            '{userName}' => '<strong>' . Html::encode($originator->displayName) . '</strong>',
-            '{answer}' => Html::encode($source->getContentDescription())
-        ));
-    ?>
-</div>
